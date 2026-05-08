ef718c2830abd5b85b8a4470984ff5267aae8a3f
Carlinhos-87 <jcpapark87@gmail.com>
2026-05-08 18:06:36 +0200

    modificaciones de texto

--- oracle-jdk8-tutorial/src/main/java/com/example/App.java
+++ oracle-jdk8-tutorial/src/main/java/com/example/App.java
@@ -10,9 +10,10 @@ public class App {
 
 	public static void main(String[] args) {
 		// System.out.println("Hello World!");
+
 		int a = 2;
 		int b = 2;
-		
+
 		sumar(a, b);
 
 		a = 3;
@@ -21,27 +22,29 @@ public class App {
 		sumar(a * 2, b * 3);
 	}
 	/*
-	 * qué es un método?: 
+	 * qué es un método?:
 	 * 
-	 * es un fragmento o bloque de codigo, de sentencias Java, en este caso, encerrado
-	 * entre llaves de apertura y cierre { } , que se declara una vez en el codigo del 
-	 * programa o que tambien podria existir en la biblioteca o API de Java, y se puede 
-	 * invocar o llamar desde muchas partes del programa pasandole o no parámetros entre
-	 *  ( ) después del nombre del fragmento de codigo
-	 *  
-	 *  Cuando el fragmento de codigo está fuera de una clase (lo cual no es posible en Java),
-	 *  se le llama subrutina o funcion, cuando está dentro de una clase se le llama método
+	 * es un fragmento o bloque de codigo, de sentencias Java, en este caso,
+	 * encerrado entre llaves de apertura y cierre { } , que se declara una vez en
+	 * el codigo del programa o que tambien podria existir en la biblioteca o API de
+	 * Java, y se puede invocar o llamar desde muchas partes del programa pasandole
+	 * o no parámetros entre ( ) después del nombre del fragmento de codigo
 	 * 
-	 * La ventaja que tiene utilizar métodos es que cuándo hay que modificarlo solamente
-	 * hay que hacerlo en un solo sitio
+	 * Cuando el fragmento de codigo está fuera de una clase (lo cual no es posible
+	 * en Java), se le llama subrutina o funcion, cuando está dentro de una clase se
+	 * le llama método
 	 * 
-	 * Y nuestro codigo de Java se inicia en un método que busca la Java Virtual Machine (JVM)
-	 * llamado método main, que podria existir varios métodos main en nuestro proyecto, pero
-	 * seria una mala practica de programacion porque habria que estar indicanco a JVM por cuál
-	 * metodo main queremos arrancar nuestro programa 
+	 * La ventaja que tiene utilizar métodos es que cuándo hay que modificarlo
+	 * solamente hay que hacerlo en un solo sitio
 	 * 
-	 * supongamos que en distintas partes del proyecto tenemos que
-	 * sumar dos operandos, es decir, dos datos que se almacenan en variables 
+	 * Y nuestro codigo de Java se inicia en un método que busca la Java Virtual
+	 * Machine (JVM) llamado método main, que podria existir varios métodos main en
+	 * nuestro proyecto, pero seria una mala practica de programacion porque habria
+	 * que estar indicanco a JVM por cuál metodo main queremos arrancar nuestro
+	 * programa
+	 * 
+	 * supongamos que en distintas partes del proyecto tenemos que sumar dos
+	 * operandos, es decir, dos datos que se almacenan en variables
 	 */
 
 }
